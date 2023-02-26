003b00242ff10aefb942798108cae9b19dad0939
XLor <yjl9903@vip.qq.com>
2023-02-26 19:51:00 +0800

    feat: export epubook

--- packages/core/test/bundle.test.ts
+++ packages/core/test/bundle.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 
-import { Epubook } from '../src/epub';
+import { Epubook } from '../src';
 import { makeContainer } from '../src/bundle';
 
 describe('Bundle Epub', () => {
