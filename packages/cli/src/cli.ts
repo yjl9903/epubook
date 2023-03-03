95be655c44bf073380678930da3abf8e8e31f258
XLor <yjl9903@vip.qq.com>
2023-03-04 05:40:45 +0800

    feat(cli): add cli option

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -7,6 +7,11 @@ const cli = breadc('epubook', {
   description: 'Generate EPUB books'
 });
 
-cli.command('').action(() => {});
+cli
+  .command('<input>', 'Bundle input document to EPUB')
+  .alias('bundle')
+  .option('-t, --theme <theme>', 'Override default theme', { default: '@epubook/theme-default' })
+  .option('-o, --output <book>', 'Output EPUB name')
+  .action((md: string) => {});
 
 cli.run(process.argv.slice(2)).catch((err) => console.error(err));
