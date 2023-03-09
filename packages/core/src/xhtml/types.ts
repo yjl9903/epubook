7940b96918302de97be889e06ec432347ef5ac7b
XLor <yjl9903@vip.qq.com>
2023-03-10 02:38:34 +0800

    feat: add div intrinsic element

--- packages/core/src/xhtml/types.ts
+++ packages/core/src/xhtml/types.ts
@@ -19,6 +19,8 @@ interface BaseElementAttrs {
 }
 
 export interface EpubIntrinsicElements {
+  div: BaseElementAttrs & {};
+
   nav: BaseElementAttrs & {};
   ul: BaseElementAttrs & {};
   ol: BaseElementAttrs & {};
